2d8e0478c2d105b9473a5f5982d28e8f3dbe54e6
Awandubelieve <awandubelieve@gmail.com>
2023-06-17 06:03:26 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unixstdio.h>
+/**
+ *main - The program prints a line to the standard error
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);	
+return (1);
+}
