206fcfdf63c6f2ce61296555d648246af8352eed
Awandubelieve <awandubelieve@gmail.com>
2023-06-20 17:56:19 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * main - A program that prints _putchar
- * Description: 'prints _putchar to stdout'
- * Return: 0 (success)
+ * main - A function that prints _putchar with a new line
+ * Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,8 @@
-#ifndef main_h
-#define main_h
-
-int _putchar(char);
+#ifndef MAIN_HEAFER_FILE
+#define MAIN_HEADER_FILE
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -13,5 +11,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+int _islower(int c);
 #endif
-
