b3653723ad6285611d6a957188d20277b0c5e874
Awandubelieve <awandubelieve@gmail.com>
2023-06-26 09:28:47 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
