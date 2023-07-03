2b2f2b8b3179da9a7a3108ffee8d84d0c9bb1d51
Awandubelieve <awandubelieve@gmail.com>
2023-07-03 14:05:57 +0100

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that get prifix substring
+ * @s: the chain
+ * @accept: the chain
+ *
+ * Return: 1 or 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (s[i] && strchr(accept, s[i]))
+		i++;
+	return (i);
+}
+
