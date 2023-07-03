fb6b63784c358d953e4a6e9cafb02438cf84dd8b
Awandubelieve <awandubelieve@gmail.com>
2023-07-03 14:50:02 +0100

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - A function that searches a string
+ * @s: the chain
+ * @accept: the chain
+ *
+ * Return: 1 or 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) || (accept == NULL))
+		return (NULL);
+	while (*s)
+	{
+		if (strchr(accept, *s))
+		{
+			return (s);
+		}
+		else
+		}
+			s++;
+		}
+	{
+	return (NULL);
+}
