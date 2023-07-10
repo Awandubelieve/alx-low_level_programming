36cc647caf2ccadf75a43ee4dcef627c9e45d67c
Awandubelieve <awandubelieve@gmail.com>
2023-07-11 00:09:41 +0100

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+/**
+ * argstostr - arguments
+ * @ac: value
+ * @av: value
+ * Return: null
+ */
+char *argstostr(int ac, char **av)
+{
+	int total_length = 0;
+
+	int i, j, k;
+
+	char *result;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			total_length++;
+			j++;
+		}
+		total_length++;
+	}
+	result = malloc((total_length + 1) * sizeof(char));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			result[k] = av[i][j];
+			j++;
+			k++;
+		}
+		result[k] = '\n';
+		k++;
+	}
+	result[k] = '\0';
+	return (result);
+}
